79d9ef00e6ce66b3c7f1bfc7b262cc653386a433
Ian <iangoodale@utexas.edu>
2022-08-09 00:05:40 -0500

    Update get_lemmas.rs

--- bin/get_lemmas.rs
+++ bin/get_lemmas.rs
@@ -1,11 +1,12 @@
 use std::env;
 
-use rust_lemmatizer::get_words;
+use rust_lemmatizer::string_or_file;
 
 pub fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
     let dict_name = &args[2];
     let file_output = &args[3];
-    get_words(filename, dict_name, file_output);
+    let input_format = &args[4];
+    string_or_file(filename, dict_name, file_output, input_format);
 }
